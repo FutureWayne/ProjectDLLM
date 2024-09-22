895ed616bb46d492ceff8c4cc8174f15b3234136
Future Wayne <futurewayne.ludens@gmail.com>
2024-09-22 04:12:10 -0600

    Add melee ability for enemy AI

---
+++ Source/Arena/Private/AbilitySystem/Ability/ArenaMeleeAbility.cpp
@@ -0,0 +1,94 @@
+// Copyright Ludens Studio. All Rights Reserved.
+
+
+#include "AbilitySystem/Ability/ArenaMeleeAbility.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "Character/ArenaCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+void UArenaMeleeAbility::TraceMeleeTarget()
+{
+	bool bHit;
+	TArray<FHitResult> HitResults;
+	AArenaCharacterBase* ArenaCharacter = GetArenaCharacterFromActorInfo();
+	FVector StartLocation = ArenaCharacter->GetActorLocation();
+	FVector EndLocation = StartLocation + ArenaCharacter->GetActorForwardVector() * MeleeRange;
+	float Radius = 96.0f;
+	float HalfHeight = 48.0f;
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(ArenaCharacter);
+	bool bTraceComplex = false;
+
+	bHit = UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		ArenaCharacter,
+		StartLocation,
+		EndLocation,
+		Radius,
+		HalfHeight,
+		ObjectTypes,
+		bTraceComplex,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		HitResults,
+		true,
+		FLinearColor::Red,
+		FLinearColor::Green,
+		2.0f
+	);
+
+	if (bHit)
+	{
+		TSet<UAbilitySystemComponent*> TargetASCSet;
+
+		// Gather ASC for hit targets
+		for (FHitResult HitResult : HitResults)
+		{
+			UAbilitySystemComponent* TargetASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(HitResult.GetActor());
+			if (!IsValid(TargetASC))
+			{
+				continue;
+			}
+
+			// TODO: Check if the hit actor and the instigator are in the same team
+
+			// Check if hit actor is behind occluding geometry
+			FHitResult OcclusionHitResult;
+			ActorsToIgnore.Add(HitResult.GetActor());
+			bool bOcclusionHit = UKismetSystemLibrary::LineTraceSingle(
+				ArenaCharacter,
+				StartLocation,
+				HitResult.ImpactPoint,
+				UEngineTypes::ConvertToTraceType(ECC_WorldDynamic),
+				false,
+				ActorsToIgnore,
+				EDrawDebugTrace::ForDuration,
+				OcclusionHitResult,
+				true,
+				FLinearColor::Red,
+				FLinearColor::Green,
+				0.5f
+			);
+
+			if (bOcclusionHit)
+			{
+				continue;
+			}
+
+			TargetASCSet.Add(TargetASC);
+		}
+
+		// Apply damage to hit targets
+		check(MeleeDamageEffectClass);
+		for (UAbilitySystemComponent* TargetASC : TargetASCSet)
+		{
+			UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo();
+			const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(MeleeDamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
+			SourceASC->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), TargetASC);
+		}
+	}
+}
--- Source/Arena/Private/AbilitySystem/ArenaGameplayAbility.cpp
+++ Source/Arena/Private/AbilitySystem/ArenaGameplayAbility.cpp
@@ -3,6 +3,46 @@
 
 #include "AbilitySystem/ArenaGameplayAbility.h"
 
+#include "AbilitySystem/ArenaAbilitySystemComponent.h"
+#include "Character/ArenaCharacterBase.h"
 
 
+AController* UArenaGameplayAbility::GetControllerFromActorInfo() const
+{
+	if (CurrentActorInfo)
+	{
+		if (AController* PC = CurrentActorInfo->PlayerController.Get())
+		{
+			return PC;
+		}
 
+		// Look for a player controller or pawn in the owner chain.
+		AActor* TestActor = CurrentActorInfo->OwnerActor.Get();
+		while (TestActor)
+		{
+			if (AController* C = Cast<AController>(TestActor))
+			{
+				return C;
+			}
+
+			if (const APawn* Pawn = Cast<APawn>(TestActor))
+			{
+				return Pawn->GetController();
+			}
+
+			TestActor = TestActor->GetOwner();
+		}
+	}
+
+	return nullptr;	
+}
+
+AArenaCharacterBase* UArenaGameplayAbility::GetArenaCharacterFromActorInfo() const
+{
+	return (CurrentActorInfo ? Cast<AArenaCharacterBase>(CurrentActorInfo->AvatarActor.Get()) : nullptr);
+}
+
+UArenaAbilitySystemComponent* UArenaGameplayAbility::GetArenaAbilitySystemComponentFromActorInfo() const
+{
+	return (CurrentActorInfo ? Cast<UArenaAbilitySystemComponent>(CurrentActorInfo->AbilitySystemComponent.Get()) : nullptr);
+}
