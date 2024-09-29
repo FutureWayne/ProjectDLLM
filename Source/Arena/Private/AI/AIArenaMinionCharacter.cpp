f0a34a43bd863c2e10008b1fec312e4fd8584097
Lutetium0 <claudeyxcai@gmail.com>
2024-09-28 21:34:43 -0600

    1. Add AIMinion Spawner 2. Allow AIMinion To Be Killed By Player 3. AIMinion Now Checks Player's Health Before Chasing Them

--- Source/Arena/Private/AI/AIArenaMinionCharacter.cpp
+++ Source/Arena/Private/AI/AIArenaMinionCharacter.cpp
@@ -4,9 +4,13 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include <Kismet/GameplayStatics.h>
 #include <Character/BlasterCharacter.h>
+#include "Character/ArenaHealthComponent.h"
+#include "Components/CapsuleComponent.h"
 
 AAIArenaMinionCharacter::AAIArenaMinionCharacter()
 {
+	HealthComponent->OnDeathStarted.AddDynamic(this, &ThisClass::OnDeathStarted);
+	HealthComponent->OnDeathFinished.AddDynamic(this, &ThisClass::OnDeathFinished);
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 	AIControllerClass = AAIArenaMinionController::StaticClass();
 	bUseControllerRotationYaw = false;
@@ -15,6 +19,35 @@ AAIArenaMinionCharacter::AAIArenaMinionCharacter()
 		GetCharacterMovement()->bUseControllerDesiredRotation = true;
 		GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
 	}
+}
+
+void AAIArenaMinionCharacter::OnDeathStarted(AActor* OwningActor)
+{
+	if (Controller)
+	{
+		Controller->SetIgnoreMoveInput(true);
+	}
+
+	UCapsuleComponent* CapsuleComp = GetCapsuleComponent();
+	check(CapsuleComp);
+	CapsuleComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	CapsuleComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+	UCharacterMovementComponent* MovementComp = GetCharacterMovement();
+	check(MovementComp);
+	MovementComp->StopMovementImmediately();
+	MovementComp->DisableMovement();
+}
+
+void AAIArenaMinionCharacter::OnDeathFinished(AActor* OwningActor)
+{
+	if (GetLocalRole() == ROLE_Authority)
+	{
+		DetachFromControllerPendingDestroy();
+		SetLifeSpan(0.1f);
+	}
+
+	SetActorHiddenInGame(true);
 }
 
 void AAIArenaMinionCharacter::SetTowerTarget(AActor* Target)
