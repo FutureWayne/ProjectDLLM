5779e31953b75bd38c752f708c962be264149edc
Wayne <futurewayne.ludens@gmail.com>
2024-11-18 02:52:35 -0700

    Add Dash Effect and some system static libraries

---
+++ Source/Arena/Private/System/ArenaSystemStatics.cpp
@@ -0,0 +1,48 @@
+// Copyright Ludens Studio. All Rights Reserved.
+
+#include "System/ArenaSystemStatics.h"
+#include "Engine/Engine.h"
+#include "Engine/World.h"
+#include "Engine/AssetManager.h"
+#include "Components/MeshComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ArenaSystemStatics)
+
+void UArenaSystemStatics::SetScalarParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const float ParameterValue, bool bIncludeChildActors)
+{
+	if (TargetActor != nullptr)
+	{
+		TargetActor->ForEachComponent<UMeshComponent>(bIncludeChildActors, [=](UMeshComponent* InComponent)
+		{
+			InComponent->SetScalarParameterValueOnMaterials(ParameterName, ParameterValue);
+		});
+	}
+}
+
+void UArenaSystemStatics::SetVectorParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const FVector ParameterValue, bool bIncludeChildActors)
+{
+	if (TargetActor != nullptr)
+	{
+		TargetActor->ForEachComponent<UMeshComponent>(bIncludeChildActors, [=](UMeshComponent* InComponent)
+		{
+			InComponent->SetVectorParameterValueOnMaterials(ParameterName, ParameterValue);
+		});
+	}
+}
+
+void UArenaSystemStatics::SetColorParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const FLinearColor ParameterValue, bool bIncludeChildActors)
+{
+	SetVectorParameterValueOnAllMeshComponents(TargetActor, ParameterName, FVector(ParameterValue), bIncludeChildActors);
+}
+
+TArray<UActorComponent*> UArenaSystemStatics::FindComponentsByClass(AActor* TargetActor, TSubclassOf<UActorComponent> ComponentClass, bool bIncludeChildActors)
+{
+	TArray<UActorComponent*> Components;
+	if (TargetActor != nullptr)
+	{
+		TargetActor->GetComponents(ComponentClass, /*out*/ Components, bIncludeChildActors);
+
+	}
+	return MoveTemp(Components);
+}
+
