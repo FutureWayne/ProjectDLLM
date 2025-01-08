5ea095f9f10f922a94e722910984c541dddc608e
Wayne <futurewayne.ludens@gmail.com>
2025-01-08 05:14:18 -0700

    message subsystem

--- Source/Arena/Arena.Build.cs
+++ Source/Arena/Arena.Build.cs
@@ -10,7 +10,7 @@ public class Arena : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "AIModule" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTasks", "ModularGameplay", "Niagara", "NetCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTasks", "ModularGameplay", "Niagara", "NetCore", "GameplayMessageRuntime" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
