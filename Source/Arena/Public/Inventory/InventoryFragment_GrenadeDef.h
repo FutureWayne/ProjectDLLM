81196f131eacdeebf24fd05f4ca1879fa74ad0fd
Wayne <futurewayne.ludens@gmail.com>
2025-01-09 10:03:37 -0700

    Adding supports for different grenade types, adding scatter grenade

---
+++ Source/Arena/Public/Inventory/InventoryFragment_GrenadeDef.h
@@ -0,0 +1,20 @@
+// Copyright Ludens Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inventory/ArenaInventoryItemDefinition.h"
+#include "InventoryFragment_GrenadeDef.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENA_API UInventoryFragment_GrenadeDef : public UArenaInventoryItemFragment
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Appearance)
+	TSubclassOf<AActor> GrenadeClass;
+};
