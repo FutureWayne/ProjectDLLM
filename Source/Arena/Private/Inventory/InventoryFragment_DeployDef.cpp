bcbed87e6f3310262e6f314f044c5a16b1fd3f58
cld <claudeyxcai@gmail.com>
2025-04-05 22:28:14 -0600

    Modify spider bot ability

---
+++ Source/Arena/Private/Inventory/InventoryFragment_DeployDef.cpp
@@ -0,0 +1,9 @@
+// Copyright Ludens Studio. All Rights Reserved.
+
+
+#include "Inventory/InventoryFragment_DeployDef.h"
+
+UArenaDeploymentDefinitionData* UInventoryFragment_DeployDef::GetDeployDefinitionData() const
+{
+	return DeployDefinitionData;
+}
